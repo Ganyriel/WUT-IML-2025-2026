6c7f3a7229d282122c088a22560f75de73b9bf48
Ganyriel <filip2.sabgol@gmail.com>
2025-11-07 10:00:06 +0100

    Added more descriptions

--- spectrogram_data.py
+++ spectrogram_data.py
@@ -7,21 +7,23 @@ import IPython.display as ipd
 # Example file
 audio_file = 'Ozymandias.wav'
 
-# Some parameters (i don't know how melspectrograms work)
-hopSize = 128
-n_fft = 2048
+# Some parameters (i don't know how melspectrograms work, i copied it from a tutorial)
+
+n_fft = 2048 # this is the number of samples in a window per fft
+hop_length = 512 # The amount of samples we are shifting after each fft
+
 
 # Extracting info from audio file
 audio_data, sampling_rate = librosa.load(audio_file, sr=44100)
 
 # Copied from tutorial for melspectrograms
-mel_signal = librosa.feature.melspectrogram(y=audio_data, sr=sampling_rate, hop_length=hopSize, 
+mel_signal = librosa.feature.melspectrogram(y=audio_data, sr=sampling_rate, hop_length=hop_length, 
  n_fft=n_fft)
 spectrogram = np.abs(mel_signal)
 power_to_db = librosa.power_to_db(spectrogram, ref=np.max)
 plt.figure(figsize=(8, 7))
 librosa.display.specshow(power_to_db, sr=sampling_rate, x_axis="time", y_axis="mel", cmap="magma", 
- hop_length=hopSize)
+ hop_length=hop_length)
 plt.colorbar(label="dB")
 plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=18))
 plt.xlabel('Time', fontdict=dict(size=15))
