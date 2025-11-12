d72a776300fbb31479ce8c8ca7321fe132e2f085
Ganyriel <filip2.sabgol@gmail.com>
2025-11-12 18:14:09 +0100

    Rewrote file to be a function

--- spectrogram_data.py
+++ spectrogram_data.py
@@ -4,28 +4,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import IPython.display as ipd
 
-# Example file
-audio_file = 'Ozymandias.wav'
 
-# Some parameters (i don't know how melspectrograms work, i copied it from a tutorial)
+# Some parameters (i don't quite know how melspectrograms work, i copied it from a tutorial)
 
 n_fft = 2048 # this is the number of samples in a window per fft
 hop_length = 512 # The amount of samples we are shifting after each fft
 
+def spec(audio_file):
+    """
+    Input: audio file
 
-# Extracting info from audio file
-audio_data, sampling_rate = librosa.load(audio_file, sr=44100)
+    Output: a plotted melspectrogram
 
-# Copied from tutorial for melspectrograms
-mel_signal = librosa.feature.melspectrogram(y=audio_data, sr=sampling_rate, hop_length=hop_length, 
- n_fft=n_fft)
-spectrogram = np.abs(mel_signal)
-power_to_db = librosa.power_to_db(spectrogram, ref=np.max)
-plt.figure(figsize=(8, 7))
-librosa.display.specshow(power_to_db, sr=sampling_rate, x_axis="time", y_axis="mel", cmap="magma", 
- hop_length=hop_length)
-plt.colorbar(label="dB")
-plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=18))
-plt.xlabel('Time', fontdict=dict(size=15))
-plt.ylabel('Frequency', fontdict=dict(size=15))
-plt.show()
+    """
+
+    # Extracting info from audio file
+    audio_data, sampling_rate = librosa.load(audio_file)
+
+    # Copied from tutorial for melspectrograms
+    mel_signal = librosa.feature.melspectrogram(y=audio_data, sr=sampling_rate, hop_length=hop_length, 
+    n_fft=n_fft)
+    spectrogram = np.abs(mel_signal)
+    power_to_db = librosa.power_to_db(spectrogram, ref=np.max)
+    plt.figure(figsize=(8, 7))
+    librosa.display.specshow(power_to_db, sr=sampling_rate, x_axis="time", y_axis="mel", cmap="magma", 
+    hop_length=hop_length,fmax=sampling_rate/2)
+    plt.colorbar(label="dB")
+    plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=18))
+    plt.xlabel('Time', fontdict=dict(size=15))
+    plt.ylabel('Frequency', fontdict=dict(size=15))
+    plt.show()
+
+
+# Example 
+audio_file = 'Ozymandias.wav'
+spec(audio_file)
+
+
+
+
